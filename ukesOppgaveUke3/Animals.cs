eca1be3219cfdcc30cd66fc0e1cbed60938df402
imanahmedoslo <imanahmedimanahmed@gmail.com>
2023-05-22 02:50:40 +0200

    Add project files.

---
+++ ukesOppgaveUke3/Animals.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ukesOppgaveUke3
+{
+    public class Animals
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public  List <string> FavoriteFood { get; private set; }
+        public string MostFavorittFood { get; private set; }
+
+        public Animals(string name, int age )
+        {
+            Name = name;
+            Age = age;
+            FavoriteFood = new List<string>();
+        }
+        public Animals(string name, int age, string mostFavorittFood)
+        {
+            Name = name;
+            Age = age;
+            MostFavorittFood = mostFavorittFood;
+            FavoriteFood = new List<string>();
+            FavoriteFood.Add(mostFavorittFood);
+            
+        }
+       
+        public void AddFood() {
+            Console.WriteLine($" choose a food to add to {this.Name} list of available foods.");
+         var food=Console.ReadLine();
+            this.FavoriteFood.Add(food);
+        }
+        public void GiveFood() {
+            Console.WriteLine($"choose what to give{this.Name} by typing the number of the food.");
+        for  (int i = 0; i < FavoriteFood.Count; i++)
+            {
+                Console.WriteLine($" number {i}: {FavoriteFood[i]}");
+            }
+            var Answer = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{this.Name} fikk {FavoriteFood[Answer]}, tusen takk!");
+
+        }
+        public void GiveFood(Animals friendAnimal)
+        {
+            Console.WriteLine($"choose what to give{this.Name} by typing the number of the food.");
+            for (int i = 0; i < FavoriteFood.Count; i++)
+            {
+                Console.WriteLine($" number {i}: {FavoriteFood[i]}");
+            }
+            var Answer = int.Parse(Console.ReadLine());
+            if (FavoriteFood[Answer]== friendAnimal.MostFavorittFood)
+            {
+                Console.WriteLine($"{this.Name} fikk {FavoriteFood[Answer]}, det er {friendAnimal.Name} sin favorittmat, yippi, nå kan de bytte!!");
+
+            } else
+            {
+                Console.WriteLine($"{this.Name} fikk {FavoriteFood[Answer]}, tusen takk!");
+            }
+            
+
+        }
+    }
+}
---
+++ ukesOppgaveUke3/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using ukesOppgaveUke3;
+
+var Cat= new Animals("cat", 3, "mice");
+var Dog= new Animals("dog", 5);
+var Giraffe= new Animals("giraffe", 7, "banana");
+var Elephant= new Animals("elephant", 33, "poop");
+List <Animals> ZooResidents= new List<Animals>();
+ZooResidents.Add(Cat);
+ZooResidents.Add(Dog);
+ZooResidents.Add(Giraffe);
+ZooResidents.Add(Elephant);
+PutFoodInFridge();
+feed();
+
+void PutFoodInFridge()
+{
+    for (int i = 0; i < ZooResidents.Count; i++)
+    {
+        ZooResidents[i].AddFood();
+    }
+    
+}
+void feed()
+{
+    
+    for (int i = 0; i < ZooResidents.Count; i++)
+    {
+        
+        Console.WriteLine(i + 1);
+        if (ZooResidents[i - 1].MostFavorittFood != null)
+        {
+            ZooResidents[i].GiveFood(ZooResidents[i-1]);
+        }
+        else ZooResidents[i].GiveFood();
+       
+    }
+}
